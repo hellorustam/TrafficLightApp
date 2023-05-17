b650ce5d422f303cde9e258b819c0f95bbd13f68
Rustam S <blakfoh@gmail.com>
2023-05-17 22:00:55 +0600

    Initial Commit

---
+++ TrafficLightApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightApp
+//
+//  Created by Rustam Samiev on 17.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
